e52d5fc31e45b91299ae1f3e91b4e7c019550f78
Ilja Booij <ilja.booij@mylaps.com>
2012-02-21 12:04:04 +0100

    QmlTest added

---
+++ QmlTest/main.cpp
@@ -0,0 +1,13 @@
+#include <QtGui/QApplication>
+#include "qmlapplicationviewer.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QmlApplicationViewer viewer;
+    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
+    viewer.setMainQmlFile(QLatin1String("qml/QmlTest/main.qml"));
+	viewer.showFullScreen();
+    return app.exec();
+}
