3f4b7d54f14406fa118e92d7c6a426b1d32bed12
Ilja Booij <ibooij@gmail.com>
2015-01-17 08:43:54 +0100

    automatic indentation fix for reallifevideoimporter.cpp

--- mainlib/reallifevideoimporter.cpp
+++ mainlib/reallifevideoimporter.cpp
@@ -18,72 +18,74 @@ RealLifeVideoImporter::RealLifeVideoImporter(QObject* parent): QObject(parent)
 }
 
 struct ParseRlvFunctor: public std::unary_function<const QString&,RealLifeVideo> {
-	ParseRlvFunctor(const QStringList& aviFiles): _aviFiles(aviFiles) {}
+    ParseRlvFunctor(const QStringList& aviFiles): _aviFiles(aviFiles) {}
 
-	RealLifeVideo operator()(const QString& filename) const {
-		QFile file(filename);
+    RealLifeVideo operator()(const QString& filename) const {
+        QFile file(filename);
         RealLifeVideo rlv = parseRealLiveVideoFile(file, _aviFiles);
         return rlv;
-	}
+    }
 
-	QStringList _aviFiles;
+    QStringList _aviFiles;
 };
 
 QStringList findFiles(const QString& root, const QString& pattern)
 {
-	QStringList filters;
-	filters << pattern;
-	QDirIterator it(root, filters, QDir::NoFilter, QDirIterator::Subdirectories);
+    QStringList filters;
+    filters << pattern;
+    QDirIterator it(root, filters, QDir::NoFilter, QDirIterator::Subdirectories);
 
-	QStringList filePaths;
-	while(it.hasNext())
-		filePaths << it.next();
-	return filePaths;
+    QStringList filePaths;
+    while(it.hasNext())
+        filePaths << it.next();
+    return filePaths;
 }
 
 QStringList findRlvFiles(QString& root)
 {
-	return findFiles(root, "*.rlv");
+    return findFiles(root, "*.rlv");
 }
 
 RealLifeVideoList importRlvFiles(QString root)
 {
-	QStringList filePaths = findRlvFiles(root);
-	QStringList aviFiles = findFiles(root, "*.avi");
+    QStringList filePaths = findRlvFiles(root);
+    QStringList aviFiles = findFiles(root, "*.avi");
 
-	QFuture<RealLifeVideo> rlvParserFuture = QtConcurrent::mapped(filePaths.begin(), filePaths.end(), ParseRlvFunctor(aviFiles));
+    QFuture<RealLifeVideo> rlvParserFuture = QtConcurrent::mapped(filePaths.begin(), filePaths.end(), ParseRlvFunctor(aviFiles));
 
-	return rlvParserFuture.results();
+    return rlvParserFuture.results();
 }
 
 void RealLifeVideoImporter::parseRealLiveVideoFilesFromDir(const QString &root)
 {
-	QFutureWatcher<RealLifeVideoList> *futureWatcher = new QFutureWatcher<RealLifeVideoList>();
-	connect(futureWatcher, SIGNAL(finished()), this, SLOT(importReady()));
-	QFuture<QList<RealLifeVideo> > importRlvFuture = QtConcurrent::run(importRlvFiles, root);
-	futureWatcher->setFuture(importRlvFuture);
+    QFutureWatcher<RealLifeVideoList> *futureWatcher = new QFutureWatcher<RealLifeVideoList>();
+    connect(futureWatcher, SIGNAL(finished()), this, SLOT(importReady()));
+    QFuture<QList<RealLifeVideo> > importRlvFuture = QtConcurrent::run(importRlvFiles, root);
+    futureWatcher->setFuture(importRlvFuture);
 }
 
 void RealLifeVideoImporter::importReady()
 {
-	QFutureWatcher<RealLifeVideoList>* watcher = dynamic_cast<QFutureWatcher<RealLifeVideoList>*>(sender());
-	if (!watcher)
-		qWarning() << "Error getting result in " << Q_FUNC_INFO;
-	RealLifeVideoList rlvList = watcher->future().result();
+    QFutureWatcher<RealLifeVideoList>* watcher = dynamic_cast<QFutureWatcher<RealLifeVideoList>*>(sender());
+    if (!watcher)
+        qWarning() << "Error getting result in " << Q_FUNC_INFO;
+    RealLifeVideoList rlvList = watcher->future().result();
 
-	// remove ergovideos and invalid rlvs
-	QMutableListIterator<RealLifeVideo> it(rlvList);
-	while(it.hasNext()) {
-		const RealLifeVideo& rlv = it.next();
-		if (!(rlv.isValid() && rlv.type() == SLOPE))
-			it.remove();
-	}
+    // remove ergovideos and invalid rlvs
+    QMutableListIterator<RealLifeVideo> it(rlvList);
+    while(it.hasNext()) {
+        const RealLifeVideo& rlv = it.next();
+        if (!(rlv.isValid() && rlv.type() == SLOPE))
+            it.remove();
+    }
 
-	// sort rlv list by name
-	qSort(rlvList.begin(), rlvList.end(), RealLifeVideo::compareByName);
+    // sort rlv list by name
+    qSort(rlvList.begin(), rlvList.end(), RealLifeVideo::compareByName);
 
-	emit importFinished(rlvList);
-	watcher->deleteLater();
+
+
+    emit importFinished(rlvList);
+    watcher->deleteLater();
 }
 
 
@@ -91,6 +93,6 @@ void RealLifeVideoImporter::importReady()
 
 RealLifeVideo parseRealLiveVideoFile(QFile &rlvFile, const QStringList& videoFilenames)
 {
-	RlvFileParser parser(videoFilenames);
-	return parser.parseRlvFile(rlvFile);
+    RlvFileParser parser(videoFilenames);
+    return parser.parseRlvFile(rlvFile);
 }
