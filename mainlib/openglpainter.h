268a144ce9d241d92036cc415ea987b54b92b819
Ilja Booij <ibooij@gmail.com>
2015-02-05 14:11:50 +0100

    reset video to black when a new video is selected.

--- mainlib/openglpainter.h
+++ mainlib/openglpainter.h
@@ -3,18 +3,18 @@
  *
  * This file is part of Big Ring Indoor Video Cycling
  *
- * Big Ring Indoor Video Cycling is free software: you can redistribute 
- * it and/or modify it under the terms of the GNU General Public License 
- * as published by the Free Software Foundation, either version 3 of the 
+ * Big Ring Indoor Video Cycling is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * Big Ring Indoor Video Cycling  is distributed in the hope that it will 
+ * Big Ring Indoor Video Cycling  is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with Big Ring Indoor Video Cycling.  If not, see 
+ * along with Big Ring Indoor Video Cycling.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
 
@@ -40,6 +40,7 @@ public:
     void paint(QPainter* painter, const QRectF& rect, Qt::AspectRatioMode aspectRatioMode);
 public slots:
     void setCurrentSample(GstSample* sample);
+    void reset();
 
 private:
     QSizeF getSizeFromSample(GstSample* sample);
