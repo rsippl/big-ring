31933d5031627a99bc57d103b80df166fbb4db3e
Ilja Booij <ibooij@gmail.com>
2015-02-05 14:12:56 +0100

    changed look of video list view. added text filter.
    Using the text filter, it's easier to find videos by name.

--- indoorcycling2/videolistview.cpp
+++ indoorcycling2/videolistview.cpp
@@ -2,7 +2,9 @@
 
 #include <QtCore/QtDebug>
 #include <QtCore/QItemSelection>
+#include <QtWidgets/QFormLayout>
 #include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QVBoxLayout>
 
 
 #include "videolistmodel.h"
@@ -10,19 +12,38 @@
 #include "videodetailswidget.h"
 
 VideoListView::VideoListView(QWidget *parent) :
-    QWidget(parent), _listView(new QListView(this)), _detailsWidget(new VideoDetailsWidget(this)), _videoListModel(new VideoListModel)
+    QWidget(parent), _filterLineEdit(new QLineEdit(this)), _listView(new QListView(this)), _filterProxyModel(new QSortFilterProxyModel(this)), _detailsWidget(new VideoDetailsWidget(this)), _videoListModel(new VideoListModel)
 {
     QHBoxLayout* layout = new QHBoxLayout;
-    _listView->setModel(_videoListModel);
+    _filterProxyModel->setSourceModel(_videoListModel);
+    _listView->setModel(_filterProxyModel);
     _listView->setItemDelegate(new VideoItemDelegate(this));
-    _listView->setMaximumWidth(500);
+    _listView->setMinimumWidth(300);
+    _listView->setSpacing(5);
     connect(_listView->selectionModel(), &QItemSelectionModel::selectionChanged, this, &VideoListView::selectionChanged);
+    connect(_listView->model(), &QAbstractItemModel::rowsInserted, _listView, [=](const QModelIndex&, int, int){
+         listViewContentChanged();
+    });
+    connect(_listView->model(), &QAbstractItemModel::rowsRemoved, _listView, [=](const QModelIndex&, int, int){
+         listViewContentChanged();
+    });
 
-    layout->addWidget(_listView);
-    layout->addWidget(_detailsWidget);
+    QFormLayout* filterFormLayout = new QFormLayout;
+    QLabel* filterLabel = new QLabel("&Filter", this);
+    filterLabel->setBuddy(_filterLineEdit);
+    filterFormLayout->addRow(filterLabel, _filterLineEdit);
+
+    QVBoxLayout* leftLayout = new QVBoxLayout;
+    leftLayout->addLayout(filterFormLayout);
+    leftLayout->addWidget(_listView);
+
+    layout->addLayout(leftLayout, 1);
+    layout->addWidget(_detailsWidget, 3);
 
     connect(_detailsWidget, &VideoDetailsWidget::playClicked, this, &VideoListView::videoSelected);
-
+    connect(_filterLineEdit, &QLineEdit::textChanged, _filterLineEdit, [=](const QString& text) {
+        _filterProxyModel->setFilterRegExp(QRegExp(text, Qt::CaseInsensitive, QRegExp::FixedString));
+    });
 
     setLayout(layout);
 }
@@ -30,15 +51,28 @@ VideoListView::VideoListView(QWidget *parent) :
 void VideoListView::setVideos(RealLifeVideoList &rlvs)
 {
     _videoListModel->setVideos(rlvs);
-    QModelIndex index = _videoListModel->index(0);
-    if (index.isValid()) {
-        _listView->selectionModel()->setCurrentIndex(index, QItemSelectionModel::Select);
-    }
+    _filterProxyModel->setSourceModel(_videoListModel);
 }
 
 void VideoListView::selectionChanged(const QItemSelection &selected, const QItemSelection &)
 {
-    qDebug() << "selection changed" << _videoListModel->data(selected.indexes()[0], Qt::DisplayRole);
-    RealLifeVideo rlv = selected.indexes()[0].data(VideoDataRole).value<RealLifeVideo>();
+    RealLifeVideo rlv;
+    if (!selected.isEmpty()) {
+        rlv = selected.indexes()[0].data(VideoDataRole).value<RealLifeVideo>();
+    }
     _detailsWidget->setVideo(rlv);
+}
+
+void VideoListView::listViewContentChanged()
+{
+    QModelIndex selectedIndex;
+    if (_listView->selectionModel()->selectedIndexes().isEmpty()) {
+        selectedIndex = _videoListModel->index(0);
+    } else {
+        selectedIndex = _listView->selectionModel()->selectedIndexes()[0];
+    }
+    if (selectedIndex.isValid()) {
+        _listView->selectionModel()->setCurrentIndex(selectedIndex, QItemSelectionModel::Select);
+        _listView->scrollTo(selectedIndex);
+    }
 }
--- indoorcycling2/videolistview.h
+++ indoorcycling2/videolistview.h
@@ -1,8 +1,10 @@
 #ifndef VIDEOLISTVIEW_H
 #define VIDEOLISTVIEW_H
 
+#include <QtCore/QSortFilterProxyModel>
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QListView>
+#include <QtWidgets/QLineEdit>
 
 class VideoListModel;
 class VideoDetailsWidget;
@@ -26,9 +28,14 @@ private slots:
     void selectionChanged(const QItemSelection & selected, const QItemSelection & deselected);
 
 private:
+    void listViewContentChanged();
+
+    QLineEdit* _filterLineEdit;
     QListView* _listView;
+    QSortFilterProxyModel* _filterProxyModel;
     VideoDetailsWidget* _detailsWidget;
     VideoListModel* _videoListModel;
+
 };
 
 #endif // VIDEOLISTVIEW_H
