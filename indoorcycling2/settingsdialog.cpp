16f0b6f41ac7ce6ae00507413da6fe241a94f26a
Ilja Booij <ibooij@gmail.com>
2015-02-14 11:48:31 +0100

    Add settings for cyclist's weight and robot cyclist.

--- indoorcycling2/settingsdialog.cpp
+++ indoorcycling2/settingsdialog.cpp
@@ -6,22 +6,43 @@
 
 SettingsDialog::SettingsDialog(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::SettingsDialog)
+    _ui(new Ui::SettingsDialog)
 {
-    ui->setupUi(this);
+    _ui->setupUi(this);
 
     QSettings settings;
-    ui->unitChooser->setCurrentText(settings.value("units").toString());
+    _ui->unitChooser->setCurrentText(settings.value("units").toString());
+    _ui->weightSpinBox->setValue(settings.value("cyclist.weight", QVariant::fromValue(82)).toInt());
+    _ui->robotCheckBox->setChecked(settings.value("useRobot", QVariant::fromValue(false)).toBool());
+    _ui->powerSpinBox->setValue(settings.value("robotPower", QVariant::fromValue(250)).toInt());
 }
 
 SettingsDialog::~SettingsDialog()
 {
-    delete ui;
+    delete _ui;
 }
 
 void SettingsDialog::on_unitChooser_currentTextChanged(const QString &choice)
 {
     QSettings settings;
     settings.setValue("units", choice);
-    qDebug() << "choosen units:" << choice;
+}
+
+void SettingsDialog::on_robotCheckBox_toggled(bool checked)
+{
+    QSettings settings;
+    settings.setValue("useRobot", QVariant::fromValue(checked));
+    _ui->powerSpinBox->setEnabled(checked);
+}
+
+void SettingsDialog::on_powerSpinBox_valueChanged(int robotPower)
+{
+    QSettings settings;
+    settings.setValue("robotPower", QVariant::fromValue(robotPower));
+}
+
+void SettingsDialog::on_weightSpinBox_valueChanged(int cyclistWeight)
+{
+    QSettings settings;
+    settings.setValue("cyclist.weight", QVariant::fromValue(cyclistWeight));
 }
--- indoorcycling2/settingsdialog.h
+++ indoorcycling2/settingsdialog.h
@@ -1,7 +1,8 @@
 #ifndef SETTINGSDIALOG_H
 #define SETTINGSDIALOG_H
 
-#include <QDialog>
+#include <QtCore/QSettings>
+#include <QtWidgets/QDialog>
 
 namespace Ui {
 class SettingsDialog;
@@ -18,8 +19,14 @@ public:
 private slots:
     void on_unitChooser_currentTextChanged(const QString &arg1);
 
+    void on_robotCheckBox_toggled(bool checked);
+
+    void on_powerSpinBox_valueChanged(int arg1);
+
+    void on_weightSpinBox_valueChanged(int arg1);
+
 private:
-    Ui::SettingsDialog *ui;
+    Ui::SettingsDialog *_ui;
 };
 
 #endif // SETTINGSDIALOG_H
