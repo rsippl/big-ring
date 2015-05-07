4ff1771b73d4b7bfefccffeef653ac924734af42
Ilja Booij <ibooij@gmail.com>
2015-05-07 20:40:53 +0200

    added github page.

--- antlib/antlib.pri
+++
@@ -1,7 +0,0 @@
-include(../config.pri)
-
-PKGCONFIG += libusb-1.0
-
-message(Including $$_FILE_ from $$IN_PWD)
-INCLUDEPATH += $$IN_PWD/
-
--- test/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "antmessage2test.h"
-
-#include <QTest>
-int main(int, char**) {
-    AntMessage2Test antMessage2Test;
-
-    QTest::qExec(&antMessage2Test);
-}
